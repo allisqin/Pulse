b33cc4c3b2fbbf5101dd2b29b2bdcad4c5bc78c4
allisqin <allisqin@gmail.com>
2023-11-06 20:59:50 +0300

    padeup

--- js/script.js
+++ js/script.js
@@ -148,5 +148,22 @@ function validateForms(form){
       });
       return false;
     });
+
+    //smooth scroll
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $(function() {
+	    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
+    });
  });
 
